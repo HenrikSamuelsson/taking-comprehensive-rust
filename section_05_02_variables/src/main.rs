975152ef9250c20af1e62efc975fc1c6bc060beb
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-06-27 04:01:53 +0200

    Add a mutable variable.

--- section_05_02_variables/src/main.rs
+++ section_05_02_variables/src/main.rs
@@ -1,4 +1,13 @@
+// Rust provides type safety via static typing.
+// Variables types are either given explicitly or inferred by the compiler.
 fn main() {
-    let x: i32 = 10;
+    // Variable bindings are made with the keyword let.
+    let x: i32 = 1;
     println!("x: {x}");
+
+    // Variable are immutable by default, the keyword mut makes variables mutable.
+    let mut y = 2;
+    println!("y: {y}");
+    y = 3;
+    println!("y: {y}");
 }
